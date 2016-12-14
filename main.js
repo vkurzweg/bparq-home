7f6c379782929e378118eebcbe982247a1df50fd
Victoria Dunham <vkurzweg@gmail.com>
2016-12-14 11:01:06 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+$( window ).scroll(function() {
+  $('#nav').css( 'background-color', 'rgba(55,55,55,0.4)' );
+});
+
+$( window ).scroll(function() {
+  $('#nav2').css( 'background-color', 'rgba(252,252,252,0.6)' );
+});
