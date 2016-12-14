bc9496cee5a2bae95b56b72272a8c7c60cb4a7a9
Victoria Dunham <vkurzweg@gmail.com>
2016-12-14 11:37:25 -0800

    reorganize files

--- test1/main.js
+++
@@ -1,7 +0,0 @@
-$( window ).scroll(function() {
-  $('#nav').css( 'background-color', 'rgba(55,55,55,0.4)' );
-});
-
-$( window ).scroll(function() {
-  $('#nav2').css( 'background-color', 'rgba(252,252,252,0.5)' );
-});
